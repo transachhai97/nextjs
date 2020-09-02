111d520339c5114effd8aa94db9f5e6bc751dce2
HaiTS <transachhai97@gmail.com>
2020-09-02 15:56:14 +0700

    source

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
+const path = require('path');
+
 module.exports = {
     // Target must be serverless
-    target: 'serverless'
+    target: 'serverless',
+    webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+        // Note: we provide webpack above so you should not `require` it
+
+        const aliases = config.resolve.alias || {};
+        config.resolve.alias = {
+            ...aliases,
+            'private-next-pages': path.resolve(__dirname, 'src/pages'),
+        };
+
+        // Important: return the modified config
+        return config;
+    },
 };
