6beabd4b1cb2486176a828388ab192a672d8a12e
HaiTS <transachhai97@gmail.com>
2020-09-03 15:06:59 +0700

    toastify

---
+++ src/components/Toastify.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+
+function Toastify() {
+    return (
+        <ToastContainer
+            newestOnTop
+        />
+    );
+}
+
+export default Toastify;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,21 @@
-import Router from 'next/router'
-import NProgress from 'nprogress'
+import Router from 'next/router';
+import NProgress from 'nprogress';
 
-import '@/styles/index.scss'
+import Toastify from '@/components/Toastify';
+
+import '@/styles/index.scss';
 
 Router.events.on('routeChangeStart', (url) => {
-  NProgress.start()
-})
-Router.events.on('routeChangeComplete', () => NProgress.done())
-Router.events.on('routeChangeError', () => NProgress.done())
+    NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <>
+        <Toastify />
+        <Component {...pageProps} />
+    </>;
 }
 
-export default MyApp
+export default MyApp;
